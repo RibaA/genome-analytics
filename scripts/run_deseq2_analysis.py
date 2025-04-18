73085600df3ef31b43c6c3b26c9c401ed719f744
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-04-18 12:15:17 -0400

    add differential expression analysis using DESeq2

--- scripts/run_deseq2_analysis.py
+++ scripts/run_deseq2_analysis.py
@@ -1,7 +1,7 @@
 """
 A simple PyDESeq2 workflow
 ===========================
-In this example, we show how to perform a simple differential expression analysis on bulk RNAseq data, using PyDESeq2.
-We start by importing required packages and setting up an optional path to save results.
+This example demonstrates how to run a basic differential expression analysis on bulk RNA-seq data using PyDESeq2. 
+We begin by importing the necessary libraries and optionally defining a path to save the results.
 
 """
