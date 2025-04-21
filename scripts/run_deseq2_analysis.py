461497ccb266c71ac6ba33e23f820c1b86c74c7c
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-04-21 15:13:32 -0400

    add data summary for synthetic data

--- scripts/run_deseq2_analysis.py
+++ scripts/run_deseq2_analysis.py
@@ -31,12 +31,28 @@ metadata = load_example_data(
     debug=False,
 )
 
-print(counts_df)
-print(metadata)
+print('\n top 5 count data: \n', counts_df.head(5))
+print('\n top 5 meta-data: \n', metadata.head(5))
+print('\n number of genes and samples: \n', counts_df.shape)
+print('\n number of samples and metadata variables: \n', metadata.shape)
 
 # data pre-processing
+metadata['condition'].unique()
+metadata['condition'].value_counts()
+metadata['condition'].isna().sum()
 samples_to_keep = ~metadata.condition.isna()
-print(samples_to_keep.value_counts())
-num_true = samples_to_keep.sum() 
-num_true = (samples_to_keep == True).sum()
+#print(samples_to_keep.value_counts())
+#num_true = samples_to_keep.sum() 
+#num_true = (samples_to_keep == True).sum()
 
+counts_df = counts_df.loc[samples_to_keep]
+counts_df.shape
+metadata = metadata.loc[samples_to_keep]
+metadata.shape
+
+genes_to_keep = counts_df.columns[counts_df.sum(axis=0) >= 10]
+counts_df = counts_df[genes_to_keep]
+
+min_val = counts_df.min().min()
+max_val = counts_df.max().max()
+print(f"Data range: {min_val} to {max_val}")
